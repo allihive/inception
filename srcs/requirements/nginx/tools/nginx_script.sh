1cdef3ed4eefc0a57117a0a841d8719016adcbf3
Alice Li Maunumäki <aliceli@Alices-MacBook-Pro.local>
2025-03-13 10:33:12 +0200

    initial nginx script written

---
+++ srcs/requirements/nginx/tools/nginx_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Is ssl certificate present?"
+
+if test -f $CERTS_KEY; then
+	echo "Yes, ssl certificate here!"
+
+else
+	echo "No, generating now..."
+	openssl req -x509 -nodes -days 365 -newkey \
+		-keyout "$CERTS_KEY" -out "$CERTS_CRT" \
+		-subj "/C=FI/ST=Uusimaa/L=Helsinki/O=42/OU=Hive/CN=alli.42.fr"
+	echo "SSL certificate generated"
+
+fi
+	#appends to a selfsigned conf
+echo "Adding SSL configuration"
+echo "ssl_certificate $CERTS_CRT;" >> /etc/nginx/snippets/self-signed.conf
+echo "ssl_certificate_key $CERTS_KEY;" >> /etc/nginx/snippets/self-signed.conf
+
+#ensuring www-data user exists
+adduser -D -H -s /sbin/nologin -g www-data -G www-data www-data
+
+echo "Starting NGINX"
+exec -g 'daemon off;'

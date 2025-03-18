8fa5cc23652a3eea447ea6530692dbcff02cc547
Alice Li <aliceli.bls@gmail.com>
2025-03-18 15:43:51 +0200

    no bash

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,18 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 #check if the directory exists
 
-if [ ! -d "/var/lib/mysql" ]; then
-	echo "Initializing MariaDB..."
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+# if [ ! -d "/var/lib/mysql" ]; then
+# 	echo "Initializing MariaDB..."
+# 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-	# echo "Starting MariaDB..."
-	# mysqld_safe --data=var/lib/mysql & #start mariadb in the background
-	# sleep 5
+# 	# echo "Starting MariaDB..."
+# 	# mysqld_safe --data=var/lib/mysql & #start mariadb in the background
+# 	# sleep 5
 
-	echo "Creating WordPress database and user..."
-	mysqld --user=mysql --bootstrap << EOF
+# 	echo "Creating WordPress database and user..."
+	
 
+# EOF
+# 	echo "Database " $WP_DATABASE_NAME " has been created successfully"
+	
+# fi
+
+mysqld --user=mysql --bootstrap <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'locahost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD:";
@@ -20,11 +26,7 @@ CREATE DATABASE IF NOT EXISTS $WP_DATABASE_NAME;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
-
 EOF
-	echo "Database " $WP_DATABASE_NAME " has been created successfully"
-	
-fi
 
 echo "Starting MariaDB..."
 exec mysqld_safe --user=mysql
